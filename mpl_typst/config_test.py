83c037001143da8ff386f2fdb63d7817d2ecf3cc
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2026-06-27 18:14:37 +0300

    Get `typst` compiler version

--- mpl_typst/config_test.py
+++ mpl_typst/config_test.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 
-from mpl_typst.config import Config
+from mpl_typst.config import Config, TypstVersion, parse_typst_compiler_version
 
 CONFIG_DICT = {
     'preamble': 'lorem ipsum',
@@ -26,3 +26,35 @@ class TestConfig:
         config = Config.from_toml(buf)
         assert config.preamble == '#let hello = ", world!"'
         assert not config.detached_images
+
+
+class TestTypstVersion:
+
+    def test_parse_release_version(self):
+        output = 'typst 0.14.2 (b33de9de)'
+        version = parse_typst_compiler_version(output)
+
+        assert version == TypstVersion(0, 14, 2, revision='b33de9de')
+        assert version.release == (0, 14, 2)
+        assert version.revision == 'b33de9de'
+        assert str(version) == '0.14.2'
+
+    def test_parse_prerelease_version(self):
+        output = 'typst 0.15.0-rc.1 (c93cf32f)'
+        version = parse_typst_compiler_version(output)
+
+        assert version == TypstVersion(0, 15, 0, ('rc', 1))
+        assert version.prerelease == ('rc', 1)
+        assert version.revision == 'c93cf32f'
+        assert str(version) == '0.15.0-rc.1'
+
+    def test_reject_invalid_version(self):
+        assert parse_typst_compiler_version('typst latest') is None
+        assert parse_typst_compiler_version('typst 0.15') is None
+        assert parse_typst_compiler_version('typst 01.15.0') is None
+
+    def test_compare_prerelease_versions(self):
+        rc1 = TypstVersion(0, 15, 0, ('rc', 1))
+        rc2 = TypstVersion(0, 15, 0, ('rc', 2))
+        release = TypstVersion(0, 15, 0)
+        assert rc1 < rc2 < release
